f80bc8341ab48ae1f46c757b278d36de29963511
WP2-dika20 <dikanugraha2088@gmail.com>
2021-11-27 07:38:18 +0700

    menambahkan membuat form register user dan membuat kategori buku

--- application/controllers/Autetifikasi.php
+++ application/controllers/Autetifikasi.php
@@ -25,6 +25,82 @@ class Autentifikasi extreds CI-Controller
             $this->_login();
         }
     }
+
+    public function registrasi()
+ {
+ if ($this->session->userdata('email')) {
+ redirect('user');
+ }
+ //membuat rule untuk inputan nama agar tidak boleh kosong 
+dengan membuat pesan error dengan 
+ //bahasa sendiri yaitu 'Nama Belum diisi'
+ $this->form_validation->set_rules('nama', 'Nama Lengkap', 
+'required', [
+ 'required' => 'Nama Belum diis!!'
+ ]);
+//membuat rule untuk inputan email agar tidak boleh kosong, 
+tidak ada spasi, format email harus valid
+ //dan email belum pernah dipakai sama user lain dengan 
+membuat pesan error dengan bahasa sendiri 
+ //yaitu jika format email tidak benar maka pesannya 'Email 
+Tidak Benar!!'. jika email belum diisi,
+ //maka pesannya adalah 'Email Belum diisi', dan jika email 
+yang diinput sudah dipakai user lain,
+ //maka pesannya 'Email Sudah dipakai'
+ $this->form_validation->set_rules('email', 'Alamat Email', 
+'required|trim|valid_email|is_unique[user.email]', [
+ 'valid_email' => 'Email Tidak Benar!!',
+ 'required' => 'Email Belum diisi!!',
+ 'is_unique' => 'Email Sudah Terdaftar!'
+ ]);
+ //membuat rule untuk inputan password agar tidak boleh 
+kosong, tidak ada spasi, tidak boleh kurang dari
+ //dari 3 digit, dan password harus sama dengan repeat 
+password dengan membuat pesan error dengan 
+ //bahasa sendiri yaitu jika password dan repeat password 
+tidak diinput sama, maka pesannya
+ //'Password Tidak Sama'. jika password diisi kurang dari 3 
+digit, maka pesannya adalah 
+ //'Password Terlalu Pendek'.
+ $this->form_validation->set_rules('password1', 'Password', 
+'required|trim|min_length[3]|matches[password2]', [
+ 'matches' => 'Password Tidak Sama!!',
+ 'min_length' => 'Password Terlalu Pendek'
+ ]);
+ $this->form_validation->set_rules('password2', 'Repeat 
+Password', 'required|trim|matches[password1]');
+ //jika jida disubmit kemudian validasi form diatas tidak 
+berjalan, maka akan tetap berada di
+ //tampilan registrasi. tapi jika disubmit kemudian validasi 
+form diatas berjalan, maka data yang 
+ //diinput akan disimpan ke dalam tabel user
+ if ($this->form_validation->run() == false) {
+ $data['judul'] = 'Registrasi Member';
+ $this->load->view('templates/aute_header', $data);
+ $this->load->view('autentifikasi/registrasi');
+ $this->load->view('templates/aute_footer');
+ } else {
+$email = $this->input->post('email', true);
+ $data = [
+ 'nama' => htmlspecialchars($this->input-
+>post('nama', true)),
+ 'email' => htmlspecialchars($email),
+ 'image' => 'default.jpg',
+ 'password' => password_hash($this->input-
+>post('password1'), PASSWORD_DEFAULT),
+ 'role_id' => 2,
+ 'is_active' => 0,
+ 'tanggal_input' => time()
+ ];
+ $this->ModelUser->simpanData($data); //menggunakan model
+ 
+ $this->session->set_flashdata('pesan', '<div 
+class="alert alert-success alert-message" role="alert">Selamat!! 
+akun member anda sudah dibuat. Silahkan Aktivasi Akun anda</div>');
+ redirect('autentifikasi');
+ }
+ }
+ 
 private function_login()
     {
         $email = htmlspecialchars($this->input->post('email',true));
@@ -40,33 +116,33 @@ private function_login()
                     $data = [
                         'email' => $user['email'],
                         'role_id' =>$user['role_id']
-                    ];                
-				 $this->session->set_userdata($data)
+                    ];
+                 $this->session->set_userdata($data)
 				 
-				 if ($user['role_id'] == 1) {
-					 redirect('admin');
-				 } else{
-					 if ($user['image'] == 'default.jpg') {
-						 $this->session->set_flashdata('pesan','<div class="alert-info alert-message" role="alert">silahkan
-						 ubah profile anda untuk ubah photo profile</div'>;
-					 }
-					           redirect('user');
-				 }
-				 } else {
-					 $this->session->set_flasdata('pesan','<div class="alert-danger aletrt-messsge" role="alert">password
-					 salah!!</div>);
-				 }
-					          redirect('autifikasi')
-				 }
-				 } else {
-					 $this->session->set_flasdata('pesan','<div class="alert-danger aletrt-messsge" role="alert">User belum
-					 diaktifasi!!</div>);
-					           redirect('autifikasi')
-				 }
-				 } else {
-					 $this->session->set_flasdata('pesan','<div class="alert-danger aletrt-messsge" role="alert">Email tidak
-					 terdaftar!!</div>);
-					 redirect('autifikasi')
-				 }
-		}
-}
+                    if ($user['role_id'] == 1) {
+                        redirect('admin');
+                    } else{
+                        if ($user['image'] == 'default.jpg') {
+                            $this->session->set_flashdata('pesan','<div class="alert-info alert-message" role="alert">silahkan
+                            ubah profile anda untuk ubah photo profile</div'>;
+                        }
+                                  redirect('user');
+                    }
+                    } else {
+                        $this->session->set_flasdata('pesan','<div class="alert-danger aletrt-messsge" role="alert">password
+                        salah!!</div>);
+                    }
+                                 redirect('autifikasi')
+                    }
+                    } else {
+                        $this->session->set_flasdata('pesan','<div class="alert-danger aletrt-messsge" role="alert">User belum
+                        diaktifasi!!</div>);
+                                  redirect('autifikasi')
+                    }
+                    } else {
+                        $this->session->set_flasdata('pesan','<div class="alert-danger aletrt-messsge" role="alert">Email tidak
+                        terdaftar!!</div>);
+                        redirect('autifikasi')
+                    }
+           }
+   }
